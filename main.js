9f676af358301c7e944aac832ea734fc2a3511e3
FedeCasper <federicorouyere@gmail.com>
2023-04-24 13:04:00 -0300

    Creating buttons and functionality

--- main.js
+++ main.js
@@ -1,34 +1,67 @@
 let url = "https://www.moogleapi.com/api/v1/characters"
-let cardsSection = document.getElementById('cardContainer')
-
+let buttonsSection = document.getElementById('buttonsContainer')
 
 fetch(url)
 .then(response => response.json())
 .then(data => {
      console.log(data);
      let arrayOrigin = [...new Set(data.map( element => element.origin))]
+     console.log(arrayOrigin);
      let arraysByGame = arrayOrigin.map( elementA => data.filter( elementB => elementB.origin == elementA ))
      console.log(arraysByGame);
 
-     function createButton (){
-          arrayOrigin.forEach( element => {
-               cardsSection.innerHTML += 
-               `
-               <button type="button" class="btn btn-primary btn-sm">${element}</button>
-               `
+     createButtons(arrayOrigin, buttonsSection)
 
-          } )
-
-     }
-     createButton()
-
-function printCards(){
-     for( array of arraysByGame){
-          for(object of array){
-               console.log(object);
-          }
-     }
-}
-
+     let buttons = document.querySelectorAll('button')
+     let arrayFilterByGame = filterByGame ( buttons , data)
 
 })
+
+function filterByGame ( buttonNodeList, originalArray ){
+     buttonNodeList.forEach( element => {
+          element.addEventListener('click', (e) => {
+               let arrayFilteredByOrigin = originalArray.filter( object => object.origin == e.target.value) 
+               console.log(arrayFilteredByOrigin);
+               printCards(arrayFilteredByOrigin)
+          } )
+     })
+}
+
+function createButtons (array, htmlElementId){
+     array.forEach( element => {
+          htmlElementId.innerHTML += 
+          `
+          <button type="button" value="${element}" class="btn btn-primary btn-sm">${element}</button>
+          `
+     } )
+}
+
+function printCards(array){
+     let cardsSection = document.getElementById('cardsContainer')
+     console.log(cardsSection);
+     let fragment = document.createDocumentFragment()
+
+     array.forEach( element => {
+          console.log("object");
+          fragment.innerHTML += 
+          `
+          <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          </ul>
+          <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+          </div>
+          </div>
+          `
+     })
+     cardsSection.appendChild(fragment)
+}
